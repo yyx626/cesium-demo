46f1b8fe1946d74b8c4628cdb27454e9c4d75a5a
yuyingxue <1605746235@qq.com>
2022-06-24 23:41:36 +0800

    清除路由a标签默认样式

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,14 @@
   </div>
 </template>
 <script>
-import Index from "./components/Index.vue";
+import Index from './components/Index.vue'
 export default {
   components: { Index },
 }
 </script>
 <style
 Index lang="scss">
-#app{
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -20,5 +20,30 @@ Index lang="scss">
   /* color: #2c3e50; */
   width: 100%;
   height: 100%;
+}
+/*包含以下五种的链接*/
+a {
+  text-decoration: none;
+  color:black;
+}
+/*正常的未被访问过的链接*/
+a:link {
+  text-decoration: none;
+}
+/*已经访问过的链接*/
+a:visited {
+  text-decoration: none;
+}
+/*鼠标划过(停留)的链接*/
+a:hover {
+  text-decoration: none;
+}
+/* 正在点击的链接，鼠标在元素上按下还没有松开*/
+a:active {
+  text-decoration: none;
+}
+/* 获得焦点的时候 鼠标松开时显示的颜色*/
+a:focus {
+  text-decoration: none;
 }
 </style>
